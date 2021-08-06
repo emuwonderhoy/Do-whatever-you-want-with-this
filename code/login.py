0d0deb97f5f8b83ed88aa7bdd23e0b42a4fd8f49
mrk61 <77993827+mrk61@users.noreply.github.com>
2021-08-06 02:46:19 -0400

    Add files via upload

---
+++ code/login.py
@@ -0,0 +1,8 @@
+#This is a normal program, so, try convert this to something bigger, this is your code.
+username = input("Enter your username: ")
+password = input(f"Enter your password: ")
+password2 = input(f"re-enter your pasword: ")
+if password == password2:
+    print("$Welcome, dear ", username)
+else:
+    print("Please, re-start the program and write correctly your password, the, you never going to register.")
