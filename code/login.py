bfc64dac9fd1dcfe167c03bbe2aeb15af09c7d61
Emu Otori <77993827+emuwonderhoy@users.noreply.github.com>
2023-11-08 00:07:04 -0300

    na

--- code/login.py
+++ code/login.py
@@ -1,8 +1,1 @@
-#This is a normal program, so, try convert this to something bigger, this is your code.
-username = input("Enter your username: ")
-password = input(f"Enter your password: ")
-password2 = input(f"re-enter your pasword: ")
-if password == password2:
-    print("$Welcome, dear ", username)
-else:
-    print("Please, re-start the program and write correctly your password, else, you never going to register.")
+
